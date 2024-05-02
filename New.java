3b62e53eab552798c16d1985efb97a4ca743b825
kkblu2 <kkblue22m2@gmail.com>
2024-05-03 00:19:38 +0530

    new

---
+++ New.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class New {
+    public static void main(String[] args) {
+        int x, y, sum;
+
+        x = 1;
+
+        y = 2;
+        sum = x + y;
+        System.out.println("Sum is: " + sum);
+    }
+}
